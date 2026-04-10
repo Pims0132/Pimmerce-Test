22ea61b22e74db1b1e0de9631a508954dd2251db
Pims0132 <106775316+Pims0132@users.noreply.github.com>
2026-04-10 11:20:55 +0200

    Delete Pimmerce-new directory

--- Pimmerce-new/app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: 'https://www.pimmerce.com', lastModified: new Date(), changeFrequency: 'monthly', priority: 1 },
-    { url: 'https://www.pimmerce.com/contact', lastModified: new Date(), changeFrequency: 'yearly', priority: 0.8 },
-  ]
-}
